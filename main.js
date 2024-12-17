bf0eeda72f1c32d6be8edf90c1f9690a85ef31b2
Sunbertus <stjhingtio@gmail.com>
2024-12-17 22:27:10 +0700

    Third Commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from './three.js-r145/build/three.module.js';
+import { OrbitControls } from './three.js-r145/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
